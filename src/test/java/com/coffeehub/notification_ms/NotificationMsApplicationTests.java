49c275c2900db710d3b2f41cd428203bda5d620f
lucasmoraist <luksmnt1101@gmail.com>
2025-08-27 18:42:54 -0300

    feat: add project

---
+++ src/test/java/com/coffeehub/notification_ms/NotificationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeehub.notification_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
